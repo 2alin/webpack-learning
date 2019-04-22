d3ad16da81fd57c54e2b03e0ff352de296fd7b52
2alin <adilsonsdev@gmail.com>
2019-04-22 02:17:02 -0500

    test inline-source-map devtool

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
