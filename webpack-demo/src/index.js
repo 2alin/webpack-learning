340a658845463771b6ea02df578966a3a1f9f25f
2alin <adilsonsdev@gmail.com>
2019-04-21 19:01:43 -0500

    add auto clean dist folder

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Pic from './pic.png';
+import printMe from './print.js'
 
 function component() {
-  let element = document.createElement('div');
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hi', 'webpack'], ' ');
@@ -14,6 +16,12 @@ function component() {
   myPic.src = Pic;
 
   element.appendChild(myPic);
+
+  // Add btn and onclick event
+  btn.innerHTML = 'Click me and check console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
